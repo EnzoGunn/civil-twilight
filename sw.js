4dbaf308c252b5b10bba55cfce527f7e88d060d8
EnzoGunn <asaeed.email@gmail.com>
2022-04-16 18:50:23 -0700

    init commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'civil-twilight-alarm';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
