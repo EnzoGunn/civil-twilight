b9e9d9d44a7a6508b1213e6bdf7cc1a4600de9ba
EnzoGunn <asaeed.email@gmail.com>
2022-05-26 20:37:08 -0700

    added basic notification support

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ window.onload = () => {
         navigator.serviceWorker.register('./sw.js');
     }
 }
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function(reg) {
+            reg.showNotification('Hello world!');
+        });
+    }
+}
